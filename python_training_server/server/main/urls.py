96b13fe604a2df9178174240516bf716b21d08b1
cybersecuritylearning <cyber.contact.learning@gmail.com>
2022-09-15 21:02:04 +0300

    Last Commit on init branch

--- python_training_server/server/main/urls.py
+++ python_training_server/server/main/urls.py
@@ -8,5 +8,6 @@ app_name = "main"
 urlpatterns = [
     path("hello/", views.hello, name="hello_page"),
     path("simple/",views.run_simple_python, name="Simple_python_module"),
-    path("get_token/",views.user_token, name="Simple_User_Token")
+    path("get_token/",views.user_token, name="Simple_User_Token"),
+    path("learn/",views.learn, name="Learn_page_for_users")
 ]
