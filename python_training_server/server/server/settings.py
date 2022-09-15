74120e3fb0b488ce27533016d3025e3a26b0aeb8
cybersecuritylearning <cyber.contact.learning@gmail.com>
2022-09-15 22:15:02 +0300

    Now template is working and this is init commmit

--- python_training_server/server/server/settings.py
+++ python_training_server/server/server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qip$wp%937vgux4elgxp&v%!1s1pd^6c9f0stlifxt*gq5w0i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
