e814bb38c89fd5b73f1537c5f2bfa0b449dcf350
cyber.contact.learning <cyber.contact.learning@gmail.com>
2023-01-22 13:27:56 -0500

    We've wrote two more modules, with post requests

--- python_training_server/server/modules/run_simple_python/first_post_request.py
+++ python_training_server/server/modules/run_simple_python/first_post_request.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-class FIRST_GET_REQUEST:
-    REQUEST_METHOD_SUPPORTED = "GET"
-    TRAIN_ID = "TRAIN-01-01"
+class FIRST_POST_REQUEST:
+    REQUEST_METHOD_SUPPORTED = "POST"
+    TRAIN_ID = "TRAIN-01-03"
     def __init__(self,request):
         self.request = request
         self.result = {"Data":"Try Harder"}
@@ -30,15 +30,15 @@ class FIRST_GET_REQUEST:
         return:
             result(dict):represents a dictionary with the result
         """
-        if self.request.method != self.REQUEST_METHOD_SUPPORTED:
-            self.result["Data"] ="You've made a POST request, not a GET request!"
+        if self.request.method !=self.REQUEST_METHOD_SUPPORTED:
+            self.result["Data"] ="You've made a GET request, not a POST request!"
             return self.result
 
         if not "python" in self.request.META['HTTP_USER_AGENT']:
             self.result["Data"] ="You have to use python!!"
             return self.result
 
-        if not len(self.request.GET):
+        if not len(self.request.POST):
             # Pass user and add the module to the passed one's list 
             if self.TRAIN_ID not in user.Passed_modules:
                 user.Passed_modules.append(self.TRAIN_ID)
---
+++ python_training_server/server/modules/run_simple_python/first_post_request_with_params.py
@@ -0,0 +1,69 @@
+"""Simple request module"""
+import os
+from hashlib import sha256
+from datetime import datetime
+
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+class FIRST_POST_REQUEST_WITH_PARAMS:
+    REQUEST_METHOD_SUPPORTED = "POST"
+    TRAIN_ID = "TRAIN-01-04"
+    def __init__(self,request):
+        self.request = request
+        self.result = {"Data":"Try Harder"}
+    
+    def make_flag(self,user):
+        """
+        params:
+            user(object):represents the object of a user (from sql)
+        return:
+            flag(str):represents a unique flag crafted
+        """
+        now = datetime.now()
+        flag = sha256(f"{str(now.microsecond)}{user.UserId}".encode()).hexdigest()
+        return flag
+
+    def process(self,user):
+        """
+        params:
+            user(object):represents the object of a user (from sql)
+        return:
+            result(dict):represents a dictionary with the result
+        """
+        if self.request.method !=self.REQUEST_METHOD_SUPPORTED:
+            self.result["Data"] ="You've made a GET request, not a POST request!"
+            return self.result
+
+        if not "python" in self.request.META['HTTP_USER_AGENT']:
+            self.result["Data"] ="You have to use python!!"
+            return self.result
+
+        if not len(self.request.POST):
+            self.request["Data"] = "You didn't provide any data!"
+            return self.result
+
+        if "code" in self.request.POST:
+            # Pass user and add the module to the passed one's list 
+            if len(self.request.POST['code']) > 5:
+                    if self.TRAIN_ID not in user.Passed_modules:
+                        user.Passed_modules.append(self.TRAIN_ID)
+
+                # Updates user database
+                    flag = self.make_flag(user)
+                    user.Hash_check = flag
+                    user.save()
+
+                    self.result["Data"] = f"""
+                            
+                                Great Job!
+                            You can proceed!
+                            Flag:{flag}
+                            
+                            """
+            else:
+                self.result["Data"] = f"You need to give a paramater longer than {len(self.request.POST['code'])}"
+        else:
+            self.result["Data"] = f"You didn't provide code data key!"
+        
+        return self.result
+
