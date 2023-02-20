3e41c46fe1fbd512b4318f2409ca903d0ac48d04
cyber.contact.learning <cyber.contact.learning@gmail.com>
2023-02-20 14:38:03 -0500

    Now we are redirected to the hello page or to the login

--- python_training_server/server/server/urls.py
+++ python_training_server/server/server/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('home/', permanent=True)),
     path("home/",include("main.urls"))
 ]
