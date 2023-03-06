534b7770c9727c1e742918596b332398c9da94a8
cybersecuritylearning <cyber.contact.learning@gmail.com>
2023-03-06 23:50:36 +0200

    Starting a docker with paramiko ssh works perfectly

--- python_training_server/server/modules/cves_modules/cve_2021_41773.py
+++ python_training_server/server/modules/cves_modules/cve_2021_41773.py
@@ -4,6 +4,6 @@ ssh = paramiko.SSHClient()
 k = paramiko.RSAKey.from_private_key_file("/Users/catalinfilip/.ssh/linode")
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname="23.92.24.245",username="root",pkey=k)
-
-print(dir(ssh))
+ssh_stdin, ssh_stdout, ssh_stderr=ssh.exec_command("docker run -p 4444:80 --rm -d vulnerable-apache")
+print(ssh_stdout.read())
 
