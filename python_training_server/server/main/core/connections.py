d1233ff7e104a2a680d6410fb2d991597e8c3837
cybersecuritylearning <cyber.contact.learning@gmail.com>
2023-03-11 09:54:38 +0200

    we've implemented a class which handles ssh connections, not tested yet

---
+++ python_training_server/server/main/core/connections.py
@@ -0,0 +1,31 @@
+import paramiko
+from utils import log_data
+
+class Connection:
+    def __init__(self,ssh_key,url,user,port=22):
+        self.url = url
+        self.port = port
+        self.ssh_key = ssh_key
+        self.user = user
+        self.ssh = paramiko.SSHClient()
+
+    def make_connection(self):
+        try:
+            
+            private_key = paramiko.RSAKey.from_private_key_file(self.ssh_key)
+            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh.connect(hostname=self.url,username=self.user,pkey=private_key)
+            return True
+
+        except Exception as e:
+            log_data.log_debug(e)
+            return False
+
+    def exec_command(self,command):
+        try:
+            ssh_stdin, ssh_stdout, ssh_stderr=self.ssh.exec_command(command)
+            return (ssh_stdin,ssh_stdout,ssh_stderr)
+        except Exception as exp:
+            log_data.log_debug(exp)
+            return False
+
