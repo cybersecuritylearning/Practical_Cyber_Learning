bc5d16fc1df2cdba1914cfc744d78d190de935d7
cybersecuritylearning <cyber.contact.learning@gmail.com>
2023-03-30 19:05:44 +0300

    added doc string to connections.py and also fixed the str port value

--- python_training_server/server/main/core/connections.py
+++ python_training_server/server/main/core/connections.py
@@ -11,8 +11,8 @@ class Connection:
         self.ssh = paramiko.SSHClient()
 
     def make_connection(self):
+        """Function which generates connection"""
         try:
-            
             private_key = paramiko.RSAKey.from_private_key_file(self.ssh_key)
             self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             self.ssh.connect(hostname=self.url,username=self.user,pkey=private_key)
@@ -23,6 +23,12 @@ class Connection:
             return False
 
     def exec_command(self,command):
+        """Function which executes a ssh command
+        params:
+            (str)command: command to exec
+        return:
+            (tupple)stds: paramiko std's pipes
+        """
         try:
             ssh_stdin, ssh_stdout, ssh_stderr=self.ssh.exec_command(command)
             return (ssh_stdin,ssh_stdout,ssh_stderr)
@@ -30,16 +36,19 @@ class Connection:
             log_data.log_debug(exp)
             return False
 
-    
-
-    def get_available_port(self):
+    def get_available_port(self)->str:
+        """Function which checks and generate port
+        return:
+                (str)port: port number as string
+        """
         try:
             
             used_ports = self.exec_command("netstat -ant |grep LISTEN |  awk '{print $4}'|awk -F \":\" '{print $NF}' | sort -u ")   
-            used_ports = used_ports[1].read().decode()
-            port = randrange(1000,10000)
+            used_ports = used_ports[1].read().decode().split("\n")
+            
+            port = str(randrange(1000,10000))
             while port in used_ports:
-                port = randrange(1000,10000)
+                port = str(randrange(1000,10000))
             
             return port
         except Exception as e:
