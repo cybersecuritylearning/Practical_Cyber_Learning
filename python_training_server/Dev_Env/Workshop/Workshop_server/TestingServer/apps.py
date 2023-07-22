60d3ab702f263f191940e8da369cc33b335aec02
cybersecuritylearning <cyber.contact.learning@gmail.com>
2023-07-22 19:19:38 +0300

    Cleaned up everything, left with just the app

--- python_training_server/Dev_Env/Workshop/Workshop_server/TestingServer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestingserverConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TestingServer'
--- python_training_server/Dev_Env/Workshop/Workshop_server/TestingServer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "TestingServer"
-
-urlpatterns = [
-    path("simple/",views.runner, name="Simple_python_module")
-]
