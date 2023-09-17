271b4c7db37f53a9b38b69342e55e8f914121bf4
cyber.contact.learning <cyber.contact.learning@gmail.com>
2023-09-17 04:47:32 -0400

    I think compatibility challange is solved, now we should focus on debbuging

--- python_training_server/src/server/main/core/utils.py
+++ python_training_server/src/server/main/core/utils.py
@@ -5,31 +5,26 @@ import json
 
 
 import logging
-import os 
+import os
 
-LOG_PATH = os.path.join("main/core/","logging/")
+LOG_PATH = "/logs/django_logs/modules.log"
 PATH_CVES_SERVERS = "main/core/"
 
-if not os.path.isdir(LOG_PATH):
-    os.mkdir(LOG_PATH)
-    LOG_PATH =os.path.join(LOG_PATH,"modules.log")
-    f=open(LOG_PATH,"w")
-    f.close()
-else:
-    LOG_PATH =os.path.join(LOG_PATH,"modules.log")
 
-logging.basicConfig(filename=LOG_PATH,
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+logging.basicConfig(
+    filename=LOG_PATH,
+    filemode="a",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.DEBUG,
+)
 
 
 class log_data:
     @staticmethod
     def log_info(value):
         logging.info(f"Value is: {value}")
-    
+
     @staticmethod
     def log_debug(value):
         logging.info(f"Error is{str(value)}")
@@ -37,64 +32,64 @@ class log_data:
 
 def dec_number_from_name(number):
     """Module which decrements by 1
-        the module number
+    the module number
     """
 
-    number = number.split('-')
-    
+    number = number.split("-")
+
     number_f = int(number[-1])
     number_f -= 1
     number_f = str(number_f)
-    
+
     if len(number_f) < 2:
-        number_f = '0'+number_f
-    number = number[0]+'-'+number[1]+'-'+ number_f
-    
+        number_f = "0" + number_f
+    number = number[0] + "-" + number[1] + "-" + number_f
+
     return number
-  
+
+
 def inc_number_from_name(number):
     """Module which increments by 1
-        the module number
+    the module number
     """
     try:
-        number = number.split('-')
-        
+        number = number.split("-")
+
         number_f = int(number[-1])
         number_f += 1
         number_f = str(number_f)
-        
+
         if len(number_f) < 2:
-            number_f = '0'+number_f
-        number = number[0]+'-'+number[1]+'-'+ number_f
+            number_f = "0" + number_f
+        number = number[0] + "-" + number[1] + "-" + number_f
     except IndexError:
-        return -1    
+        return -1
     return number
-  
+
 
 def init_userToken(request):
     User_data = UserToken()
 
     now = datetime.now()
     user_token = sha256(str(now.microsecond).encode()).hexdigest()
-    
+
     User_data.User = request.user
-    User_data.Score = 0 
+    User_data.Score = 0
     User_data.UserId = user_token
     User_data.save()
 
     return User_data
 
+
 class CVEsAndServers:
     @staticmethod
     def get_server(CVE):
-       servers_and_cves = os.path.join(PATH_CVES_SERVERS,"servers_cves")
-        
-       with open(servers_and_cves,"r") as file:
-           servers = file.readlines()[3:]
-        
-       for server in servers:
-           server = json.loads(server)
-           if server['CVE_NUMBER'] == CVE:
-               return server["SERVER"]
-    
-    
+        servers_and_cves = os.path.join(PATH_CVES_SERVERS, "servers_cves")
+
+        with open(servers_and_cves, "r") as file:
+            servers = file.readlines()[3:]
+
+        for server in servers:
+            server = json.loads(server)
+            if server["CVE_NUMBER"] == CVE:
+                return server["SERVER"]
--- python_training_server/src/server/server/settings.py
+++ python_training_server/src/server/server/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qip$wp%937vgux4elgxp&v%!1s1pd^6c9f0stlifxt*gq5w0i0'
+SECRET_KEY = "django-insecure-qip$wp%937vgux4elgxp&v%!1s1pd^6c9f0stlifxt*gq5w0i0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,52 +32,52 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions'
+    "main.apps.MainConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'server.urls'
+ROOT_URLCONF = "server.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'server.wsgi.application'
+WSGI_APPLICATION = "server.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
@@ -93,16 +93,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -122,39 +122,37 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"static/")
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGGING = {
-    "version": 1,  # the dictConfig format version
-    "disable_existing_loggers": False,  # retain the default loggers
-    "formatters": {
-        "verbose": {
-            "format": "{name} {levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
-        },
-        "simple": {
-            "format": "{levelname} {message}",
-            "style": "{",
-        },
-    },
-
-    "handlers": {
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": "main_views.log",
-            "formatter":"verbose"
-        },
-    },
-     "loggers": {
-        "main.views": {
-            "level": "WARNING",
-            "handlers": ["file"],
-},
-    },
-
-}
+# LOGGING = {
+#     "version": 1,  # the dictConfig format version
+#     "disable_existing_loggers": False,  # retain the default loggers
+#     "formatters": {
+#         "verbose": {
+#             "format": "{name} {levelname} {asctime} {module} {process:d} {thread:d} {message}",
+#             "style": "{",
+#         },
+#         "simple": {
+#             "format": "{levelname} {message}",
+#             "style": "{",
+#         },
+#     },
+#     "handlers": {
+#         "file": {
+#             "class": "logging.FileHandler",
+#             "filename": "/server/logs/main_views.log",
+#             "formatter": "verbose",
+#         },
+#     },
+#     "loggers": {
+#         "main.views": {
+#             "level": "WARNING",
+#             "handlers": ["file"],
+#         },
+#     },
+# }
