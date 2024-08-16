17668758fd8e5b63b882290c1722f149ab1bfbea
cybersecuritylearning <cyber.contact.learning@gmail.com>
2024-08-17 00:21:34 +0300

    added a dashboard page with training categories

--- python_training_server/src/server/main/views.py
+++ python_training_server/src/server/main/views.py
@@ -341,11 +341,14 @@ def docker(request):
 
 
 def template_test(request):
-    if not 'mod_name' in request.GET:
-        return render(request=request,template_name='main/template_test.html',context={"Modules":Learning_Modules.objects.all()})
+    # if not 'mod_name' in request.GET:
+    #     return render(request=request,template_name='main/template_test.html',context={"Modules":Learning_Modules.objects.all()})
     
-    User = UserToken.objects.filter(User=request.user)[0]
-    User.current_level = request.GET['mod_name']
-    User.save()
+    # User = UserToken.objects.filter(User=request.user)[0]nique_categ
+    # User.current_level = request.GET['mod_name']
+    # User.save()
     
-    return redirect("main:Learn_page_for_users")
+    categories = Learning_Modules.objects.values_list("Module_type",flat=True)
+
+    unique_categ = list(set(categories))
+    return render(request=request,template_name='main/dashboard.html',context={"categs":unique_categ})
---
+++ python_training_server/static/main/js/custom.js
@@ -0,0 +1,111 @@
+
+  
+    
+    
+    $(document).ready(function() {
+    
+      
+
+    const button = document.getElementById('Flag-btn');
+    var flag_input = document.getElementById('flag');
+
+    button.addEventListener('click', async _ => {
+        try {     
+              $.ajax({
+                type: "POST",
+                url: "/home/learn/",
+                headers: {
+                  "X-CSRFToken": getCookie("csrftoken")
+                },
+                mode: "same-origin",
+                data: {
+                  'Flag': flag_input.value // from form
+                },
+                success:function(json){
+                  
+                  $("#flag").val('');
+                  $("#quest_message").html(json.quest);
+                  $("#Instance-btn").html(json.instance);
+                  $("#quest_tip").html(json.tips);
+                  $("#solved").html(json.done);
+                  try {
+                    if (json.fail){
+                    M.toast({html: json.fail, classes: 'red rounded'});
+                    }
+                    else{
+                    M.toast({html: 'Great Job!', classes: 'green rounded'});  
+                    setInterval(function() {
+                      location.reload();
+                    }, 3000);
+                  }
+                  
+                  }
+                  catch(err) {
+                    console.log("It's correct");
+                  }
+
+                }
+              
+        });
+          } catch(err) {
+            console.error(`Error: ${err}`);
+          }
+        
+    });
+
+    const prev = document.getElementById('prev');
+    prev.addEventListener('click', async _ => {
+      try {    
+            
+            
+            $.ajax({
+              type: "GET",
+              url: "/home/move/",
+              mode: "same-origin",
+              data: {
+                'pos': 'prev' // from form
+              },
+              success:function(json){
+                $("#flag").val('');
+                $("#solved").html(json.done);
+                $("#quest_message").html(json.quest);
+                $("#quest_tip").html(json.tips);
+                $("#docker_start").html(json.instance);
+              }
+            
+      });
+        } catch(err) {
+          console.error(`Error: ${err}`);
+        }
+      
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', async _ => {
+    try { 
+          
+          $.ajax({
+            type: "GET",
+            url: "/home/move/",
+            mode: "same-origin",
+            data: {
+              'pos': 'next' // from form
+            },
+            success:function(json){
+             
+              $("#flag").val('');
+              $("#solved").html(json.done);
+              $("#quest_message").html(json.quest);
+              $("#quest_tip").html(json.tips);
+              $("#docker_start").html(json.instance);
+            }
+            
+          
+    });
+
+      } catch(err) {
+        console.error(`Error: ${err}`);
+      }
+    
+});
+    });
